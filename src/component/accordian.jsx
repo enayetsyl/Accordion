dbc49ce33679434c1e0e4dc573bee1118abffdb0
Enayet <enayetflweb@gamil.com>
2024-03-01 10:32:45 +0600

    single open accordion design done done

--- src/component/accordian.jsx
+++ src/component/accordian.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import accordionData from "../../public/accordianData.json";
-
+import './accordion.css'
 const Accordion = () => {
   const [selected, setSelected] = useState(null);
   console.log(selected);
@@ -10,16 +10,25 @@ const Accordion = () => {
   };
 
   return (
-    <div className="container">
-      <div className="wrapper">
+    <div className="flex justify-center items-center min-h-screen">
+      <div >
         {accordionData && accordionData.length > 0 ? (
           accordionData.map((data) => (
-            <div key={data.id}>
-              <h3 onClick={() => handleAccordionSelect(data.id)}>
+            <div key={data.id}
+            className={`border-2 border-sky-500 p-3 mb-2 rounded-lg ${selected === data.id ? 'bg-blue-100' : ''} `}
+            >
+              <div className="flex justify-between items-start cursor-pointer gap-5 transition ease-in 3s"
+               onClick={() => handleAccordionSelect(data.id)}>
+              <h3 
+              className={` ${selected === data.id ? 'font-bold' : ''} pb-3`}
+             >
                 {data.question}
               </h3>
-              <span>+</span>
-              {selected === data.id ? <p>{data.answer}</p> : null}
+              <span className="text-3xl">+</span>
+              </div>
+              {selected === data.id ? <p
+              className="transition ease-in 3s"
+              >{data.answer}</p> : null}
             </div>
           ))
         ) : (
